72102f08847e3bd244840f54936d5759fa723156
Julia Leite <julia.lsilva@usp.br>
2020-05-24 17:02:39 -0300

    Pelo menos a matriz gerada agora não é igual a identidade

--- compress.m
+++ compress.m
@@ -8,18 +8,26 @@ function compress (originalImg, k)
   I = imread (originalImg, "PixelRegion", {[0 746],[0 746]});
 
   p = 745; % pixels da imagem do cupcake
-  n = floor ((p + k) / (k + 1)); % imagem comprimida será n x n
+  n = floor ((p + k) / (k + 1)) % imagem comprimida será n x n
   
   img = eye (n); % imagem comprimida
-
-  for i = 1:1:n+1
-    if mod (i-1,k+1) == 0
-      for j = 1:1:n+1
-        if mod (j-1,k+1) == 0
-          img((i-1)/(k+1) + 1,(j-1)/(k+1) + 1) = I(i,j);
-        endif
-      endfor
-    endif
+  
+  l = 1; % indica a linha na matriz original 
+  c = 1; % indica a coluna
+  
+  % percorremos as linhas da nova imagem
+  for i = 1:1:n
+    % percorremos também as colunas dessa nova imagem    
+    for j = 1:1:n
+      
+      img (i,j) = I(l,c);
+      c += k+1; % avançamos para o próximo múltiplo de k+1
+      
+    endfor
+    
+    l += k+1; % avançamos para a próxima linha
+    c = 1; % voltamos a coluna pro começo
+    
   endfor
   img
 endfunction
