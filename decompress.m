022483f43ca9e764b1a38f86da437315e26ae39e
Lara Ayumi <laraayumi56@gmail.com>
2020-05-27 22:23:44 -0300

    Mudanças feitas em decompress.

--- decompress.m
+++ decompress.m
@@ -2,12 +2,11 @@
 
 function decompress(compressedImg, method, k, h)
  
-  # Para receber o colormap exato da imagem:
-  # uso o imread, recebendo o cmap e img
+  # Para receber o colormap exato da imagem uso o imread, recebendo o cmap e img
   [img, cmap] = imread(compressedImg);
   
   # Tamanho da imagem
-  [img_width, img_height] = size(img); 
+  [img_width, img_height, num] = size(img); 
   
   # Requisito para rodar esta função:
   if(img_width == img_height)
@@ -17,38 +16,94 @@ function decompress(compressedImg, method, k, h)
     p = n + (n-1)*k;
     printf("O valor gerado para p é %d.\n", p);
     
-    #new_img = eye (p); % nova imagem descomprimida
-    new_img = zeros(p);
+    new_img = zeros(p, 'uint8');
     
-    l = 1; % indica a linha na matriz original 
-    c = 1; % indica a coluna
+    # Lógica análoga à do programa anterior
+    # irei retirar o código abaixo, porque nçao vou usar
+    l = c = 1;
     
-    # Percorre apenas valores da matriz original
     for i = 1:1:n
       for j = 1:1:n
         new_img (l,c) = img(i,j);
-        c += k+1; % avançamos para o próximo múltiplo de k+1  
+        c += k+1;
       endfor
-      l += k+1; % avançamos para a próxima linha
-      c = 1; % voltamos a coluna pro começo
+      
+      l += k+1;
+      c = 1;
     endfor
     
-    # Desta forma, a matriz resultante possui, nos k espaços
-    # entre as linhas e colunas originais, 0s como elementos
+    # Desta forma, a matriz resultante possui 0s como elementos nos k espaços 
+    # entre as linhas e colunas originais.
     
-    #imshow(new_img, cmap);
-    
-    # Agora falta interpolar os pontos entre (i,j)
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+    # Interpolação por partes:
     
     switch(method)
       case 1 # Bilinear
         disp("Método 1: Interpolação bilinear ");
+        # Criação das matrizes contendo valores das componentes RGB
+        imshow(new_img); % isto será retirado dps
         
-        rgb = img;
+        # Conceder valor para eles nas respectivas posições
+        RED = img(:,:,1);     # Red (n pixels)
+        GREEN = img(:,:,2);   # Green (n pixels)
+        BLUE = img(:,:,3);    # Blue (n pixels)
         
-        RED = rgb(:,:,1);
-        imshow(RED);
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        new_R = zeros(p, 'uint8');
         
+        l = c = 1;
+        
+        for i = 1:1:n
+          for j = 1:1:n
+            new_R (l,c) = img(i,j);
+            c += k+1;
+          endfor
+          
+          l += k+1;
+          c = 1;
+        endfor
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        new_G = zeros(p, 'uint8');
+                
+        l = c = 1;
+        
+        for i = 1:1:n
+          for j = 1:1:n
+            new_G (l,c) = img(i,j);
+            c += k+1;
+          endfor
+          
+          l += k+1;
+          c = 1;
+        endfor
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        new_B = zeros(p, 'uint8');
+                
+        l = c = 1;
+        
+        for i = 1:1:n
+          for j = 1:1:n
+            new_B (l,c) = img(i,j);
+            c += k+1;
+          endfor
+          
+          l += k+1;
+          c = 1;
+        endfor
+        
+        %%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
+        # Primeiro vou iterar sobre a imagem original e, começando do elemento
+        # (1,1), checo os "quadrados" de pixels mais próximos, ou seja, na primeira
+        # iteração será (1,1), (1,2), (2,1) e (2,2). Preciso também checar se os
+        # quadrados abaixo existem (ou seja, < n) e os do lado direito também.
+        # Ao entrar em cada posição, preciso iterar sobre os pixels dentro desse
+        # quadrado, ou seja, de (1,1) até (2+k, 2+k).
+        
+        # Para cada uma dessas iterações, 
         
         #(xi, yj ), tal que
         # xi = x + ih, x E R
@@ -62,13 +117,17 @@ function decompress(compressedImg, method, k, h)
         
       case 2 # Bicúbico
         disp("Método 2: Interpolação bicúbica ");
+        # implementação
+        
       otherwise # ??
         disp("Método inválido...");
-    endswitch
         
-    
+    endswitch
+    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+        
   else
     disp("A imagem inserida não é quadrada. Por favor insira uma imagem nxn.");
+    
   endif
   
 endfunction
