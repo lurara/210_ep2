f171f72638e6a88f6650603ae1c76943b7731b71
Lara Ayumi <laraayumi56@gmail.com>
2020-06-07 17:50:55 -0300

    Feita para preto2

--- coloridas.m
+++ coloridas.m
@@ -1,6 +1,5 @@
 % f1 (x,y) = (255*sin(x/y) , 255*cos(x-y), 255*sin(x*y))
 % f2 (x,y) = (sin (x²+y²), 200, y/x)
-% f3 (x,y) = ( y / x, 500 /(x+y), ln (x/y) )
 
 function coloridas 
   p = 500
@@ -10,20 +9,31 @@ function coloridas
   
   for x = 1:p
     for y = 1:p
+      % [-1,1]
+      aux = sin(x*x+y*y);
       
+      % Para que esteja entre 0 a 255, temos:
+      aux = 255*aux;
+      aux = aux + 255;
+      aux =  aux/2;
       
+      r(x,y) = aux;
       
+      b(x,y) = 200;
       
-      %g(x,y) = (255-x-y);
-      %r(x,y) = 255 - x + y;
-      %b(x,y) = 500 - x - y;
+      %[0.00000..., 1] imagem quadrada
+      aux = y/x;
       
+      % Para que esteja entre 0 a 255, temos:
+      aux = 255*aux;
+      
+      g(x,y) = aux;
       
     endfor
   endfor
   
   img = cat (3, r, g, b);
   imshow (img);
-  imwrite (img, 'cor_1.png', 'Quality', 100);
+  imwrite (img, 'cor_2.png', 'Quality', 100);
 
 endfunction
